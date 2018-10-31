617462f48825d824f72ea30e16bfad85201633cd
Zach Frank <zachf714@gmail.com>
2018-10-31 19:13:37 +0000

    Setup basic input form for user

--- activities/app/controllers/active_controller.rb
+++ activities/app/controllers/active_controller.rb
@@ -1,7 +1,17 @@
 class ActiveController < ApplicationController
+    
     def new
     end
     
     def create
+        @user = User.new(params[user_params])
+        
+        @user.save
+        redirect_to @user
     end
 end
+
+private
+    def user_params
+        params.requre(:active).permit(:title, :text)
+    end
