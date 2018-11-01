527c1bc7f1b03c7983716a0bccfdb8e51d557be5
Zach Frank <zachf714@gmail.com>
2018-11-01 15:00:26 +0000

    Add "new_active_path" to new.html.erb

--- activities/app/controllers/active_controller.rb
+++ activities/app/controllers/active_controller.rb
@@ -1,17 +1,30 @@
 class ActiveController < ApplicationController
     
+    def index
+        @preload = Preload.all
+        @user = User.all
+    end
+    
     def new
     end
     
+    def show
+        @preload = Preload.find(params[:id])
+        @user = User.find(params[:id])
+    end
+    
     def create
-        @user = User.new(params[user_params])
+        @user = User.new(user_params)
         
-        @user.save
-        redirect_to @user
+        if @user.save
+            redirect_to @user
+        else
+            render 'new'
+        end
     end
 end
 
 private
     def user_params
-        params.requre(:active).permit(:title, :text)
+        params.requre(:user).permit(:title, :text)
     end
