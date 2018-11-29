381f48bbd3c384801b3552da6088c9acd62a87b1
Caleb Earney <cearney@uncc.edu>
2018-11-29 15:51:45 +0000

    Fixed associated model deletion for user activities

--- activities/app/controllers/users_controller.rb
+++ activities/app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
         redirect_to preload_path(@preload)
     end
     
+    def destroy
+        @preload = Preload.find(params[:id])
+        @user = @preload.users.find(params[:id])
+        @user.destroy
+        
+        redirect_to preload_index_path
+    end
+    
     private
     def user_params
         params.require(:user).permit(:zip, :body)
